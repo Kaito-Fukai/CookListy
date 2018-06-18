357c0b903769dbdc653e59d19236b518fa1c6dfe
Kaito-Fukai <yaiag04@gmail.com>
2018-06-18 06:15:21 +0000

    fixed transaction

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
-  before_action :ransack
   protect_from_forgery with: :exception
+  before_action :ransack
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def ransack
   	@q = Recipe.ransack(params[:q])
@@ -10,5 +11,9 @@ class ApplicationController < ActionController::Base
   private
   def search_params
   	params.require(:q).permit!
+  end
+
+  def configure_permitted_parameters
+  	devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana])
   end
 end
